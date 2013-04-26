e6173a497ffd32534a58e566cce54f13c0d96229
Joel VanderWerf <vjoel@users.sourceforge.net>
2013-04-26 13:58:19 -0700

    added message class and tests

---
+++ lib/funl/message.rb
@@ -0,0 +1,90 @@
+module Funl
+  class Message
+    # Unique (per funl instance) sequential id of client who sent message.
+    attr_accessor :client_id
+
+    # Client's sequential id of message, unique only in client scope.
+    attr_accessor :local_tick
+
+    # Global sequential id of message, unique in scope of funl instance.
+    # In client request to funl, this means last ack-ed global tick.
+    # Assummed to be 64 bits, to avoid rollover errors.
+    attr_accessor :global_tick
+
+    # In client request, how far ahead of ack this message is. When
+    # messages are pipelined, delta > 1.
+    attr_accessor :delta
+
+    attr_accessor :tags
+
+    # Application-dependent payload data.
+    attr_accessor :blob
+    
+    def initialize(*args)
+      @client_id, @local_tick, @global_tick, @delta, @tags, @blob = *args
+    end
+
+    def inspect
+      d = delta ? "+#{delta}" : nil
+      t = tags ? " #{tags}" : nil
+      s = [
+        "client #{client_id}",
+        "local #{local_tick}",
+        "global #{global_tick}#{d}"
+      ].join(", ")
+      "<Message: #{s}#{t}>"
+    end
+    
+    def to_a
+      [@client_id, @local_tick, @global_tick, @delta, @tags, @blob]
+    end
+
+    def == other
+      other.kind_of? Message and
+        @client_id = other.client_id and
+        @local_tick = other.local_tick and
+        @global_tick = other.global_tick and
+        @delta = other.delta and
+        @tags = other.tags and
+        @blob = other.blob
+    end
+    alias eql? ==
+    
+    def hash
+      @client_id.hash ^ @local_tick.hash ^ @global_tick.hash
+    end
+    
+    # Call with Packer, nil, or IO. If +pk+ is nil, returns string. If +pk+ is
+    # a Packer, returns the Packer, which will need to be flushed. If +pk+ is
+    # IO, returns nil.
+    def to_msgpack(pk = nil)
+      case pk
+      when MessagePack::Packer
+        pk.write_array_header(6) ## redundant, unless omit delta / tags
+        pk.write @client_id
+        pk.write @local_tick
+        pk.write @global_tick
+        pk.write @delta
+        pk.write @tags
+        pk.write @blob
+        return pk
+      
+      else # nil or IO
+        MessagePack.pack(self, pk)
+      end
+    end
+    
+    def self.from_msgpack(src)
+      case src
+      when MessagePack::Unpacker
+        new(*src.read) ## do this without allocating array?
+      
+      when IO, StringIO
+        from_msgpack(MessagePack::Unpacker.new(src))
+      
+      else # String
+        from_msgpack(MessagePack::Unpacker.new.feed(src))
+      end
+    end
+  end
+end
---
+++ test/test-message.rb
@@ -0,0 +1,69 @@
+require 'funl/message'
+require 'socket'
+require 'stringio'
+require 'msgpack'
+
+require 'minitest/autorun'
+
+class TestMessage < MiniTest::Unit::TestCase
+  attr_reader :m, :sio
+  
+  def setup
+    @m = Funl::Message.new
+    m.client_id = 12
+    m.local_tick = 34
+    m.global_tick = 56
+    m.delta = 78
+    m.tags = [ "aaa", "bbb" ]
+    m.blob = "BLOB"
+  end
+  
+  def test_to_msgpack
+    assert_kind_of(String, m.to_msgpack(nil))
+    assert_kind_of(String, m.to_msgpack)
+    assert_equal(MessagePack.pack(m), m.to_msgpack(nil))
+    assert_equal(MessagePack.pack(m), m.to_msgpack)
+  end
+  
+  def test_to_msgpack_packer
+    pk = MessagePack::Packer.new
+    assert_kind_of(MessagePack::Packer, m.to_msgpack(pk))
+    assert_equal(pk, m.to_msgpack(pk))
+  end
+  
+  def test_to_msgpack_io
+    src, dst = UNIXSocket.pair
+    assert_nil(m.to_msgpack(src))
+    src.close
+    assert_equal(MessagePack.pack(m), dst.read)
+  end
+  
+  def test_pack_io
+    src, dst = UNIXSocket.pair
+    MessagePack.pack(m, src)
+    src.close
+    assert_equal(MessagePack.pack(m), dst.read)
+  end
+  
+  def test_pack_unpack
+    assert_equal(m.to_a, MessagePack.unpack(MessagePack.pack(m)))
+  end
+  
+  def test_from_msgpack_str
+    assert_equal(m, Funl::Message.from_msgpack(MessagePack.pack(m)))
+  end
+  
+  def test_from_msgpack_stringio
+    sio = StringIO.new
+    MessagePack.pack(m, sio)
+    sio.rewind
+    assert_equal(m, Funl::Message.from_msgpack(sio))
+  end
+  
+  def test_from_msgpack_io
+    src, dst = UNIXSocket.pair
+    MessagePack.pack(m, src)
+    src.close
+    assert_equal(m, Funl::Message.from_msgpack(dst))
+  end
+end
