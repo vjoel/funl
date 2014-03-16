6768322b1880e6dbd47a68febc8dae6208068ead
Joel VanderWerf <vjoel@users.sourceforge.net>
2014-03-16 12:44:39 -0700

    pass stream opts to object-stream

--- lib/funl/stream.rb
+++ lib/funl/stream.rb
@@ -8,15 +8,15 @@ module Funl
 
   # Mixin depends on stream_type, log, client_id, message_class.
   module Stream
-    def client_stream_for io, type: stream_type
-      ObjectStreamWrapper.new(io, type: type).tap do |stream|
+    def client_stream_for io, type: stream_type, **opts
+      ObjectStreamWrapper.new(io, type: type, **opts).tap do |stream|
         stream.write_to_outbox {{"client_id" => client_id}}
           # client_id will be nil in the case of cseq, but that's ok.
       end
     end
 
-    def server_stream_for io, type: stream_type
-      ObjectStreamWrapper.new(io, type: type).tap do |stream|
+    def server_stream_for io, type: stream_type, **opts
+      ObjectStreamWrapper.new(io, type: type, **opts).tap do |stream|
         stream.consume do |h|
           raise StreamError, "bad handshake: #{h.class}" unless h.kind_of? Hash
           client_id = h["client_id"]
@@ -26,8 +26,8 @@ module Funl
       end
     end
     
-    def message_server_stream_for io, type: stream_type
-      ObjectStreamWrapper.new(io, type: type).tap do |stream|
+    def message_server_stream_for io, type: stream_type, **opts
+      ObjectStreamWrapper.new(io, type: type, **opts).tap do |stream|
         stream.consume do |h|
           raise StreamError, "bad handshake: #{h.class}" unless h.kind_of? Hash
           client_id = h["client_id"]
